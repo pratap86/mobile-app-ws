93f7dc86ef7eba7cfce354cde931082d8663f76d
pratap86 <narayanpratap86@gmail.com>
2020-04-24 23:35:57 +0530

    tomcat deployment deature implemented

--- src/main/java/com/pratap/app/ws/MobileAppWsApplication.java
+++ src/main/java/com/pratap/app/ws/MobileAppWsApplication.java
@@ -3,18 +3,27 @@ package com.pratap.app.ws;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.pratap.app.ws.security.AppProperties;
 
 @SpringBootApplication
-public class MobileAppWsApplication {
+public class MobileAppWsApplication extends SpringBootServletInitializer {
+	
+	
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MobileAppWsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppWsApplication.class, args);
 	}
-	
+
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -24,12 +33,12 @@ public class MobileAppWsApplication {
 	public SpringApplicationContext springApplicationContext() {
 		return new SpringApplicationContext();
 	}
-	
+
 	@Bean
 	public AppProperties getAppProperties() {
 		return new AppProperties();
 	}
-	
+
 	@Bean
 	public ModelMapper modelMapper() {
 		ModelMapper modelMapper = new ModelMapper();
